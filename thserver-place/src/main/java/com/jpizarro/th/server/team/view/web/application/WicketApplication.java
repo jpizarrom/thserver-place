2c5ca358c576e10ff06e5d32440446a0c13f4af5
Juan Pizarro <jpizarro@doingit.cl>
2011-02-06 07:39:11 -0300

    skel min

--- thserver-place/src/main/java/com/jpizarro/th/server/team/view/web/application/WicketApplication.java
+++ thserver-place/src/main/java/com/jpizarro/th/server/team/view/web/application/WicketApplication.java
@@ -7,6 +7,7 @@ import org.apache.wicket.Session;
 import org.apache.wicket.authentication.AuthenticatedWebApplication;
 import org.apache.wicket.authentication.AuthenticatedWebSession;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.protocol.http.WebRequestCycleProcessor;
 import org.apache.wicket.protocol.http.request.CryptedUrlWebRequestCodingStrategy;
 import org.apache.wicket.protocol.http.request.WebRequestCodingStrategy;
@@ -21,7 +22,7 @@ import org.wicketstuff.annotation.scan.AnnotatedMountScanner;
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
  */
 @Component
-public class WicketApplication extends AuthenticatedWebApplication
+public class WicketApplication extends WebApplication
 { 
 //	@Autowired
 //	private UserServiceAuthenticationManager userServiceAuthenticationManager;
@@ -45,53 +46,8 @@ public class WicketApplication extends AuthenticatedWebApplication
 		// TODO Auto-generated method stub
 		super.init();
 		
-		/* WS */
-//		mountBookmarkablePage("/ws/login", LoginWS.class);
-//		mountBookmarkablePage("/ws/logout", LogoutWS.class);
-//		mountBookmarkablePage("/ws/updateLocation", UpdateLocationWS.class);
-//		mountBookmarkablePage("/ws/updatePersonalInfo", .class);
-//		mountBookmarkablePage("/ws/register", .class);
-//		mountBookmarkablePage("/ws/changePassword", .class);
-//		mountBookmarkablePage("/ws/getPublicUserProfile", .class);
-		
-//		mountBookmarkablePage("/ws/findGameById", FindGameByIdWS.class);
-//		mountBookmarkablePage("/ws/findCitiesWithGames", FindCitiesWithGamesWS.class);
-//		mountBookmarkablePage("/ws/findGamesByCity", FindGamesByCityWS.class);
-//		mountBookmarkablePage("/ws/findGamesByLocation", .class);
-		
-//		mountBookmarkablePage("/ws/findTeamById", FindTeamByIdWS.class);
-//		mountBookmarkablePage("/ws/findTeamsByGame", FindTeamsByGameWS.class);
-		
-//		mountBookmarkablePage("/ws/joinGame", JoinGameWS.class);
-//		mountBookmarkablePage("/ws/abandonGame", AbandonGameWS.class);
-//		mountBookmarkablePage("/ws/GameState", GameStateWS.class);
-//		mountBookmarkablePage("/ws/takePlace", TakePlaceWS.class);
-		
-//		mountBookmarkablePage("/ws/sendMessage", SendMessageWS.class);
-//		mountBookmarkablePage("/ws/findMessages", .class);
-
-		/* Web Pages*/
-//		mountBookmarkablePage("/user/home", UserHomePage.class);
-//		mountBookmarkablePage("/login", LoginPage.class);
-//		mountBookmarkablePage("/register", RegisterPage.class);
-		
-//		mountBookmarkablePage("/createGame", .class);
-//		mountBookmarkablePage("/editGame", .class);
-//		mountBookmarkablePage("/gameDetails", GameDetailsPage.class);
-//		mount(new MixedParamUrlCodingStrategy("gameDetails", GameDetailsPage.class,
-//                new String[] {"gameId"}));
-
-//		mountBookmarkablePage("/watchGame", .class);
-//		mountBookmarkablePage("/activeGames", .class);
-//		mountBookmarkablePage("/notFinishedGames", NotFinishedGamesListPage.class);
-//		mountBookmarkablePage("/finishedGames", .class);
-
 		new AnnotatedMountScanner().scanPackage("com.jpizarro.th.server").mount(this);
 
-		
-		/* Testing Web Pages*/
-//		mountBookmarkablePage("/ws/game/GameTest", GameTestWS.class);
-		
 //		getApplicationSettings().setAccessDeniedPage(LoginPage.class);
 		/*
          * Remove wicket tags from result HTML
@@ -137,31 +93,5 @@ public class WicketApplication extends AuthenticatedWebApplication
 	public static WicketApplication get() {
         return (WicketApplication) Application.get();
     }
-
-	@Override
-	protected Class<? extends WebPage> getSignInPageClass() {
-		// TODO Auto-generated method stub
-//		return AnonymousHomePage.class;
-		return null;
-	}
-
-	@Override
-	protected Class<? extends AuthenticatedWebSession> getWebSessionClass() {
-		// TODO Auto-generated method stub
-//		return WicketSession.class;
-		return null;
-	}
-	
-//	public UserService getUserService() {
-//		return userService;
-//	}
-
-//	public GameService getGameService() {
-//		return gameService;
-//	}
-
-//	public UserServiceAuthenticationManager getUserServiceAuthenticationManager() {
-//		return userServiceAuthenticationManager;
-//	}
 
 }
