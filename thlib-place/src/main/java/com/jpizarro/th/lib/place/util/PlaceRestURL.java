6447048bc734c9bea3ca66f378ac35a215793a1b
Juan Pizarro <jpizarro@doingit.cl>
2011-02-14 10:06:20 -0300

    add url rest

---
+++ thlib-place/src/main/java/com/jpizarro/th/lib/place/util/PlaceRestURL.java
@@ -0,0 +1,9 @@
+package com.jpizarro.th.lib.place.util;
+
+
+public class PlaceRestURL {
+
+	public static final String ENTITY = "places";
+	
+	public static final String ENTITY_ID = "/{id}";
+}
--- thserver-place/src/main/java/com/jpizarro/th/server/place/view/rest/PlaceController.java
+++ thserver-place/src/main/java/com/jpizarro/th/server/place/view/rest/PlaceController.java
@@ -3,6 +3,7 @@ package com.jpizarro.th.server.place.view.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,22 +11,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.jpizarro.th.lib.place.entity.PlaceTO;
+import com.jpizarro.th.lib.place.util.PlaceRestURL;
 import com.jpizarro.th.server.generic.model.persistence.util.exceptions.DuplicateInstanceException;
 import com.jpizarro.th.server.generic.model.persistence.util.exceptions.InstanceNotFoundException;
+import com.jpizarro.th.server.generic.util.ResourceNotFoundException;
 import com.jpizarro.th.server.generic.view.rest.GenericController;
 import com.jpizarro.th.server.place.model.service.PlaceService;
 
 @Controller
-@RequestMapping("/places")
+@RequestMapping(PlaceRestURL.ENTITY)
 public class PlaceController implements GenericController <PlaceTO, Long>{
 	@Autowired
 	private PlaceService placeService;
 	private String XML_VIEW_NAME = "users";
 	
-	@RequestMapping(method=RequestMethod.GET, value="/{id}",
+	@RequestMapping(method=RequestMethod.GET, value=PlaceRestURL.ENTITY_ID,
 			headers="Accept=application/xml")
 	@ResponseBody
 	public PlaceTO getEntity(@PathVariable Long id) {
@@ -35,15 +39,17 @@ public class PlaceController implements GenericController <PlaceTO, Long>{
 		} catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw new ResourceNotFoundException();
 		} catch (InstanceNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw new ResourceNotFoundException();
 		}
 		return to;
 //		return new ModelAndView(XML_VIEW_NAME, BindingResult.MODEL_KEY_PREFIX+"object", to);
 	}
 	
-	@RequestMapping(method=RequestMethod.DELETE, value="/{id}")
+	@RequestMapping(method=RequestMethod.DELETE, value=PlaceRestURL.ENTITY_ID)
 	public ModelAndView removeEntity(@PathVariable Long id) {
 		boolean ret = true;
 		try {
@@ -52,6 +58,7 @@ public class PlaceController implements GenericController <PlaceTO, Long>{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			ret = false;
+			throw new ResourceNotFoundException();
 		}
 		
 		return new ModelAndView(XML_VIEW_NAME, BindingResult.MODEL_KEY_PREFIX+"user", ret);
@@ -64,7 +71,7 @@ public class PlaceController implements GenericController <PlaceTO, Long>{
 	}
 
 	@Override
-	@RequestMapping(method=RequestMethod.PUT, value="/{id}")
+	@RequestMapping(method=RequestMethod.PUT, value=PlaceRestURL.ENTITY_ID)
 	@ResponseBody
 	public PlaceTO updateEntity(@PathVariable Long id, @RequestBody PlaceTO entity) {
 		// TODO Auto-generated method stub
@@ -74,13 +81,15 @@ public class PlaceController implements GenericController <PlaceTO, Long>{
 		} catch (InstanceNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw new ResourceNotFoundException();
 		}
-		return null;
+//		return null;
 	}
 
 	@Override
 	@RequestMapping(method=RequestMethod.POST)
 	@ResponseBody
+	@ResponseStatus(value=HttpStatus.CREATED)
 	public PlaceTO addEntity(@RequestBody PlaceTO body) {
 		// TODO Auto-generated method stub
 		PlaceTO r =null;
